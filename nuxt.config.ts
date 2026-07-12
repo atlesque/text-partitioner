b9dbd227af0ad7ebe061dbc097270641fdb4d6cb
Alex Van Maele <alexander.vanmaele@vlaanderen.be>
2026-07-12 18:17:10 +0200

    chore: remove SSR disabled flag from Nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,6 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  ssr: false,
   modules: ['@nuxt/ui'],
   css: ['~/assets/css/main.css'],
   devServer: { port: 8040 },
